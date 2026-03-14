6c1b80ccc6e7e24d8d868d86961b8fc6171f94ac
Leander Rijneveld <leanderrijneveld@monotch.com>
2026-03-14 14:04:03 +0000

    OPNsense bootstrap script

--- b.sh
+++ b.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 # b.sh — OPNsense bootstrap
-# Usage: setenv GITHUB_USER xxx && fetch -qo - https://raw.githubusercontent.com/monotch-cluster-provisioning/opnsense/main/b.sh | sh
-
-# Guard: fail clearly if no user provided
-if [ -z "$GITHUB_USER" ]; then
-  echo "ERROR: GITHUB_USER not set. Run as: setenv GITHUB_USER xxx && fetch -qo - <url> | sh"
-  exit 1
-fi
-
-# SSH keys from GitHub
-mkdir -p /root/.ssh
-fetch -qo /root/.ssh/authorized_keys \
-  https://github.com/${GITHUB_USER}.keys
-chmod 700 /root/.ssh
-chmod 600 /root/.ssh/authorized_keys
+# Usage: fetch -qo - https://raw.githubusercontent.com/monotch-cluster-provisioning/opnsense/main/b.sh | sh
 
 # Update config.xml to enable SSH
 /usr/local/bin/php -r '
